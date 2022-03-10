16aa4b167acbc72ed99865a92be68c5be57c7e30
jrbeverly <jonathan@jrbeverly.me>
2022-03-10 02:38:23 +0000

    Camera + board

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -9,8 +9,40 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+                Vec3::new(-7.0, 20.0, 4.0),
+            )),
+            ..Default::default()
+        }
+    );
+
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1. }));
+    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
+
+    // Spawn 64 squares
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn_bundle(PbrBundle {
+                mesh: mesh.clone(),
+                // Change material according to position to get alternating pattern
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                ..Default::default()
+            });
+        }
+    }
 }
 
 fn change_clear_color(input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>) {
